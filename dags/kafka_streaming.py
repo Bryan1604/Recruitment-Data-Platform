528734bf37db8625cb5718b79a85be250f1ba4b8
Bryan1604 <luongvuhs@gmail.com>
2024-01-02 16:40:29 +0700

    add spark, kafka, mongo

--- dags/kafka_streaming.py
+++ dags/kafka_streaming.py
@@ -9,6 +9,7 @@ default_args = {
     'start_date': datetime(2023, 12, 18, 10, 00)
 }
 
+#read json data file
 def read_json_file(file_name, **kwargs):
     import logging
     base_path = os.path.dirname(os.path.abspath(__file__))
@@ -22,15 +23,15 @@ def read_json_file(file_name, **kwargs):
 
 companies_data = read_json_file('companies.json')
 jobs_data = read_json_file('jobs.json')
-#stream job data
+
+#stream job data to broker browser
 def stream_job_data(**kwargs):
     import json
     import time
     import logging
     from kafka import KafkaProducer
 
-    producer = KafkaProducer(
-                bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    producer = KafkaProducer( bootstrap_servers=['broker:29092'], max_block_ms=5000)
     curr_time = time.time()
     while True:
         if time.time() > curr_time + 60: # 1 minute
@@ -39,7 +40,7 @@ def stream_job_data(**kwargs):
             if 'jobs' in kwargs:
                 listJob = kwargs['jobs']
          
-            data_length = len(listJob)  # Avoid using built-in names like 'len'
+            #data_length = len(listJob)  # Avoid using built-in names like 'len'
             for job in listJob:
                 producer.send('jobs', json.dumps(job).encode('utf-8'))
                 
@@ -47,14 +48,14 @@ def stream_job_data(**kwargs):
         except Exception as e:
             logging.error(f'Error in stream_company_data: {e}', exc_info=True)
 
+#stream company data to broker browser
 def stream_company_data(**kwargs):
     import json
     import time
     import logging
     from kafka import KafkaProducer
 
-    producer = KafkaProducer(
-                bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     curr_time = time.time()
     while True:
         if time.time() > curr_time + 60: # 1 minute
@@ -63,7 +64,7 @@ def stream_company_data(**kwargs):
             if 'companies' in kwargs:
                 listCompany = kwargs['companies']
          
-            data_length = len(listCompany)  # Avoid using built-in names like 'len'
+            #data_length = len(listCompany)
             for company in listCompany:
                 producer.send('companies', json.dumps(company).encode('utf-8'))
                 
@@ -89,17 +90,19 @@ with DAG('crawl_job_automation',
         provide_context=True,
         op_kwargs={'file_name': 'jobs.json'}
     )
-    streaming_company = PythonOperator(
-        task_id = 'stream_company_data',
-        python_callable = stream_company_data,
-        provide_context=True,
-        op_kwargs={'companies': companies_data}
-    )
+    
     streaming_job = PythonOperator(
         task_id = 'stream_job_data',
         python_callable = stream_job_data,
         provide_context=True,
         op_kwargs={'jobs': jobs_data}
+    )
+    
+    streaming_company = PythonOperator(
+        task_id = 'stream_company_data',
+        python_callable = stream_company_data,
+        provide_context=True,
+        op_kwargs={'companies': companies_data}
     )
 
 [read_company_file, read_job_file] >> streaming_job >> streaming_company
